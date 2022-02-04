fe77203064bb1ef35b5a0e1a99ba375e181532a8
SheilaNS <shei.nsantos@gmail.com>
2022-02-04 03:29:27 -0300

    Requisito 5 ok

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const cart = document.querySelector('.cart__items'); // cart ol
 const clearCart = document.querySelector('.empty-cart'); // clear button
-const cartSection = document.querySelector('.cart'); // cart section
+const priceSpan = document.querySelector('.total-price'); // span de preço
+let preco = 0;
 
+// cria o elemento da imagem
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -9,6 +11,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// cria um elemento HTML
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -16,6 +19,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// cria os items da section items
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -28,6 +32,7 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+// adiciona os items no HTML
 const showItems = async () => {
   const itemObj = await fetchProducts('computador');
   const { results } = itemObj;
@@ -38,15 +43,25 @@ const showItems = async () => {
   });
 };
 
+// seleciona o ID do item
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// apaga o li do cart
 function cartItemClickListener(event) {
   event.target.remove();
   saveCartItems(cart.innerHTML);
+  preco = Number(priceSpan.innerText);
+  itemPrice = event.target.innerText;
+  arrayPrice = itemPrice.split(' ');
+  priceOnly = Number(arrayPrice[arrayPrice.length - 1].slice(1));
+  console.log(typeof preco);
+  console.log(typeof priceOnly);
+  priceSpan.innerText = +(+preco - +priceOnly).toFixed(2);
 }
 
+// cria o elemento do cart
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -55,42 +70,36 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-const itemPrice = async (event) => {
-  const itemObj = await fetchItem(getSkuFromProductItem(event.target.parentNode));
-  const { price } = itemObj;
-  cartSection.appendChild(createCustomElement('span', 'item-price', Number(price)));
-};
-
-const sumPrice = () => {
-  const totalSpan = document.createElement('span');
-  totalSpan.className = 'total-price';
-  totalSpan.innerText = 0;
-  cartSection.appendChild(totalSpan);
-};
-
-// const itemsPrice = document.querySelectorAll('.item-price');
-// totalSpan.innerText = itemsPrice.forEach((item) => total + Number(item));
-
+// adiciona os items do cart no HTML
+priceSpan.innerText = preco;
 const createCart = async (event) => {
   const itemObj = await fetchItem(getSkuFromProductItem(event.target.parentNode));
   const { id, title, price } = itemObj;
   cart.appendChild(createCartItemElement({ sku: id, name: title, salePrice: price }));
   saveCartItems(cart.innerHTML);
+  preco += Number(price);
+  priceSpan.innerText = +preco;
 };
 
+// função escutador do botão esvaziar carrinho
 const clearCartItems = () => {
   localStorage.removeItem('cartItems');
   cart.innerHTML = '';
 };
-
 clearCart.addEventListener('click', clearCartItems);
 
+// carrega a página ao atualizar
 window.onload = async () => {
   cart.innerHTML = getSavedCartItems();
   cart.childNodes.forEach((item) => item.addEventListener('click', cartItemClickListener));
   await showItems();
   const addButtons = document.querySelectorAll('.item__add');
   addButtons.forEach((button) => button.addEventListener('click', createCart));
-  addButtons.forEach((button) => button.addEventListener('click', itemPrice));
-  sumPrice();
 };
+
+/* 
+  requisito pede: soma dos itens no carrinho
+  <div class='container-cartTitle'>
+    <p class='total-price'>0</p>
+  </div>
+*/
